99e5a0f5f652ca728b14e099b71c74c79413568a
Willlumm <william.lumme@gmail.com>
2023-11-20 15:05:37 +0000

    Implemented password generator page

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from password_generator import generate_password
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("main.html")
+
+@app.route("/password")
+def password():
+    password = generate_password()
+    return render_template("password.html", password=password)
