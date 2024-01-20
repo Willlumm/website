030317ecec73100798d750b38f4529146f647191
Willlumm <william.lumme@gmail.com>
2024-01-20 20:29:53 +0000

    Generate bracket function

--- tourney.py
+++ tourney.py
@@ -19,22 +19,30 @@ def arrange_seed(n):
         seeds = new_seeds
     return seeds
 
-items = [i for i in range(1, 2**8+1)]
-print(arrange_seed(len(items)))
-
-"""
 def generate_bracket(items):
-    # round, a_name, b_name, a_score, b_score, destination
+    # match, round, a_name, b_name, a_score, b_score, destination
     bracket = []
     i = len(items)
     r = 0
     while i > 1:
         round = []
         for j in range(i//2):
-            round.append([r, None, None, None, None, i//2 + (j - i//2)])
+            a, b = None, None
+            dest = len(bracket) + i//2 + (j//2)
+            if r == 0:
+                a, b = items[2*j : 2*j+2]
+            elif i == 2:
+                dest = None
+            round.append([r, a, b, None, None, dest])
+            print(round[-1])
         i //= 2
         r += 1
+        bracket.extend(round)
+        print()
     return bracket
+
+items = [i for i in range(1, 2**6+1)]
+generate_bracket(items)
 
 def get_votes(match):
     print(f"{match[0]} vs {match[1]}")
@@ -48,7 +56,7 @@ def main(items):
         match[2:] = get_votes(match)
     
 
-
+"""
 items = ["a", "b", "c", "d"]
 main(items)
 
