727e2bb11f70ada585e9aae2685e3fe44c4265cb
Willlumm <william.lumme@gmail.com>
2024-01-24 21:03:44 +0000

    Local tourney prototype

--- tourney.py
+++ tourney.py
@@ -3,11 +3,6 @@
 # Get result of match
 # Update state of tourney
 
-# 1: 0
-# 2: 1
-# 4: 3
-# 8: 7
-
 import json
 import math
 
@@ -26,60 +21,41 @@ def generate_bracket(items):
         bracket_len = len(bracket)
         round_len = len(items) // 2**round
         for i in range(round_len):
-            left, right = None, None
-            next_match = bracket_len + round_len + i//2
+            game_items = []
+            next_game = bracket_len + round_len + i//2
             if round == 1:
-                left, right = items[2*i : 2*i+2]
+                game_items = items[2*i : 2*i+2]
             elif round_len == 1:
-                next_match = None
+                next_game = None
             bracket.append({
-                "match": bracket_len + i,
+                "game": bracket_len + i,
                 "round": round,
-                "left": left,
-                "right": right,
-                "left_score": None,
-                "right_score": None,
-                "next_match": next_match
+                "items": game_items,
+                "scores": [],
+                "next_game": next_game
             })
-            print(bracket[-1])
-        print()
     return bracket
 
 def save_bracket(bracket):
     with open("bracket.json", "w") as file:
         json.dump(bracket, file)
 
-items = [i for i in range(1, 2**3+1)]
-bracket = generate_bracket(items)
-save_bracket(bracket)
-
-def get_votes(match):
-    print(f"{match[0]} vs {match[1]}")
-    votes = input.split(" ")
-    return votes
+def get_scores(items):
+    print(" vs ".join([str(item) for item in items]))
+    scores = [int(score) for score in input().split(" ")]
+    return scores
         
-
 def main(items):
     bracket = generate_bracket(items)
-    for match in bracket:
-        match[2:] = get_votes(match)
-    
+    save_bracket(bracket)
+    for game in bracket:
+        scores = get_scores(game["items"])
+        game["scores"] = scores
+        winner = game["items"][scores.index(max(scores))]
+        next_game = game["next_game"]
+        if next_game:
+            bracket[next_game]["items"].append(winner)
+        save_bracket(bracket)
 
-"""
-items = ["a", "b", "c", "d"]
+items = [i for i in range(1, 2**3+1)]
 main(items)
-
-while len(items) > 1:
-
-    winners = []
-    mid = len(items) // 2
-
-    for a, b in zip(items[:mid], items[mid:]):
-        if "1" == "1":
-            winners.append(a)
-        else :
-            winners.append(b)
-
-    items = winners
-    print(items)
-"""
