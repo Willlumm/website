ca41bde94cb8d5294d42233da41bf966c8aa8472
Willlumm <william.lumme@gmail.com>
2024-02-08 17:38:16 +0000

    Civ leader scraper before run

--- util/civ_scraper.py
+++ util/civ_scraper.py
@@ -6,16 +6,35 @@ url = "https://civilization.fandom.com/wiki/Leaders_(Civ6)"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
+
+imgs = soup.find_all("img", attrs={"alt": True, "data-src": True, "data-relevant": 1})
+for i, img in enumerate(imgs):
+    end = img["data-src"].index("scale-to-width-down")
+    url = img["data-src"][:end]
+    img_response = requests.get(url, stream=True)
+    img_response.raw.decode_content = True
+
+    filename = f"static/img/{img["alt"]}.png"
+    with open(filename, "wb") as file:
+        shutil.copyfileobj(img_response.raw, file)
+    
+    print(f"Scraping image {i} of {len(imgs)} ({img["alt"]})")
+    
+"""
 tables = soup.find_all("table", class_="article-table")
 
 for table in tables:
     for tr in table.tbody:
-        print(tr)
-        break
+        tr[0]
+        
+        for td in tr:
+            print(td)
+            print("--------------------------------------")
+        print("\n########################################\n")
     break
 
 print(len(soup.find_all("table", class_="article-table")))
-
+"""
 # response = requests.get("https://civilization.fandom.com/wiki/Civilization_VI?file=Frederick_Barbarossa_%28Civ6%29.png")
 
 """
