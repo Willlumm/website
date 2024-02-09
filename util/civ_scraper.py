3a7896e3bdc29a5da741748fc267647f7ebdaf08
Willlumm <william.lumme@gmail.com>
2024-02-09 16:35:53 +0000

    Adapt scraper for maps

--- util/civ_scraper.py
+++ util/civ_scraper.py
@@ -3,31 +3,37 @@ import requests
 import shutil
 
 url = "https://civilization.fandom.com/wiki/Leaders_(Civ6)"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
 
-tables = soup.find_all("table", class_="article-table")
+def scrape_imgs_from_tables(url, dir, height=None):
 
-for i, table in enumerate(tables):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    imgs = table.find_all("img", attrs={"alt": True, "data-src": True})
+    tables = soup.find_all("table", class_="article-table")
 
-    print(f"\nSearching table {i+1} of {len(tables)}\n")
+    for i, table in enumerate(tables):
 
-    for j, img in enumerate(imgs):
+        imgs = table.find_all("img", attrs={"alt": True, "data-src": True})
 
-        if img["height"] != "44":
-            print(f"Image {j+1} skipped")
-            continue
+        print(f"\nSearching table {i+1} of {len(tables)}\n")
 
-        end = img["data-src"].index("scale-to-width-down")
-        img_url = img["data-src"][:end]
+        for j, img in enumerate(imgs):
 
-        img_response = requests.get(img_url, stream=True)
-        img_response.raw.decode_content = True
+            if img["height"] != str(height):
+                print(f"Image {j+1} skipped")
+                continue
 
-        filename = f"static/img/{img["alt"]}.png"
-        with open(filename, "wb") as file:
-            shutil.copyfileobj(img_response.raw, file)
-        
-        print(f"Scraping image {j+1} of {len(imgs)} ({img["alt"]})")
+            end = img["data-src"].index("scale-to-width-down")
+            img_url = img["data-src"][:end]
+
+            img_response = requests.get(img_url, stream=True)
+            img_response.raw.decode_content = True
+
+            filename = f"static/img/{dir}/{img["alt"]}.png"
+            with open(filename, "wb") as file:
+                shutil.copyfileobj(img_response.raw, file)
+            
+            print(f"Scraping image {j+1} of {len(imgs)} ({img["alt"]})")
+
+# scrape_imgs_from_tables("https://civilization.fandom.com/wiki/Leaders_(Civ6)", "leaders", height=44)
+scrape_imgs_from_tables("https://civilization.fandom.com/wiki/Map_(Civ6))", "maps", height=48)
