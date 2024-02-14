03e2bbe754f58437a1d355d052e24db2d20095ef
Willlumm <william.lumme@gmail.com>
2024-02-14 23:18:37 +0000

    Options for victory conditions and game modes, UI improvements, refactoring

--- main.py
+++ main.py
@@ -12,12 +12,30 @@ def main():
 
 @app.route("/civ", methods=["GET", "POST"])
 def civ():
-    if request.form:
-        settings = request.form
-    else:
-        settings = None
-    result = civ_randomizer.randomize(settings)
-    return render_template("civ.html", settings=settings, result=result)
+    if request.method == "GET":
+        options = {
+            "victory_condition_count":      "1",
+            "game_mode_min":                "0",
+            "game_mode_max":                "8",
+            "Culture":                      "random",
+            "Diplomatic":                   "random",
+            "Domination":                   "random",
+            "Religious":                    "random",
+            "Science":                      "random",
+            "Score":                        "never",
+            "Apocalypse":                   "random",
+            "Barbarian Clans":              "random", 
+            "Dramatic Ages":                "random", 
+            "Heroes & Legends":             "random", 
+            "Monopolies and Corporations":  "random", 
+            "Secret Societies":             "random", 
+            "Tech and Civic Shuffle":       "random",
+            "Zombie Defense":               "random"
+        }
+    elif request.method == "POST":
+        options = request.form
+    result = civ_randomizer.randomize(options)
+    return render_template("civ.html", options=options, result=result)
 
 @app.route("/password", methods=["GET", "POST"])
 def password():
